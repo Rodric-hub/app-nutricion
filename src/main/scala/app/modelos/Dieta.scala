1e0905f8e8e5315adad69b07230c7e70aad5b9af
César Jamil Suazo Romero <suazoromerojamil@gmail.com>
2026-06-07 02:04:15 -0500

    "chore: añadiendo más alimentos a Dieta.scala"

--- src/main/scala/app/modelos/Dieta.scala
+++ src/main/scala/app/modelos/Dieta.scala
@@ -5,3 +5,60 @@ case class Dieta(
     calorias: Int,
     alimentos: List[String]
 )
+
+object Dieta {
+  val dietaPerder: Dieta = Dieta(
+    nombre = "Pérdida de peso",
+    calorias = 1500,
+    alimentos = List(
+      "Pechuga de pollo a la plancha",
+      "Ensalada de espinacas con tomate",
+      "Avena con agua",
+      "Manzana",
+      "Yogur natural sin azúcar",
+      "Brócoli al vapor",
+      "Claras de huevo",
+      "Pepino",
+      "Zanahoria",
+      "Té verde sin azúcar"
+    )
+  )
+
+  val dietaMantener: Dieta = Dieta(
+    nombre = "Mantenimiento",
+    calorias = 2000,
+    alimentos = List(
+      "Arroz integral",
+      "Pechuga de pavo",
+      "Lenteja guisada",
+      "Plátano",
+      "Huevo entero",
+      "Pan integral",
+      "Leche descremada",
+      "Quinoa",
+      "Atún en agua",
+      "Naranja",
+      "Camote sancochado",
+      "Almendras"
+    )
+  )
+
+  val dietaGanar: Dieta = Dieta(
+    nombre = "Ganancia muscular",
+    calorias = 2800,
+    alimentos = List(
+      "Arroz blanco",
+      "Filete de res",
+      "Huevos enteros",
+      "Avena con leche",
+      "Papa sancochada",
+      "Pechuga de pollo",
+      "Mantequilla de maní",
+      "Pan integral con atún",
+      "Frijoles negros",
+      "Aguacate",
+      "Leche entera",
+      "Nueces"
+    )
+  )
+}
